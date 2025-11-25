98e34912a7bcbb718a087621b1b8f8db094340d9
yannmm <yannmm@foxmail.com>
2025-11-25 23:06:42 +0800

    Work on refuel log form.

--- lib/view/refuel_log_form.dart
+++ lib/view/refuel_log_form.dart
@@ -1,37 +1,104 @@
+import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:tdesign_flutter/tdesign_flutter.dart';
 import 'package:tianqi/view/refuel_form1.dart';
+import 'dart:math' as math;
 
 class RefuelLogForm extends StatefulWidget {
-  const RefuelLogForm({super.key});
-
   @override
   State<RefuelLogForm> createState() => _RefuelLogFormState();
 }
 
 class _RefuelLogFormState extends State<RefuelLogForm> {
-  // @override
-  // Widget build(BuildContext context) => TDCollapse.accordion(
-  //       style: TDCollapseStyle.block,
-  //       expansionCallback: (int index, bool isExpanded) {
-  //         // setState(() {
-  //         //   _accordionData[index].isExpanded = !isExpanded;
-  //         // });
-  //       },
-  //       children: kSections.map((item) {
-  //         return TDCollapsePanel(
-  //           headerBuilder: (BuildContext context, bool isExpanded) {
-  //             return Text(item);
-  //           },
-  //           isExpanded: true,
-  //           body: const Text('xxxxklkjsldfjsdf sklfjklsdfjlsf ksjfllsjdf'),
-  //           value: item,
-  //         );
-  //       }).toList(),
-  //     );
+  final _controller = ExpandableController(initialExpanded: false);
+
+  final _section1Title = ReplaySubject<String>(maxSize: 1);
 
   @override
-  Widget build(BuildContext context) => SizedBox.shrink();
+  void initState() {
+    _controller.addListener(() {
+      _section1Title
+          .add(_controller.expanded ? 'Refuel Log Details' : 'Refuel Log');
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    buildItem(String label) {
+      return Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Text(label),
+      );
+    }
+
+    buildList() {
+      return Column(
+        children: <Widget>[
+          for (var i in [1, 2, 3, 4]) buildItem("Item ${i}"),
+        ],
+      );
+    }
+
+    return ExpandableNotifier(
+        child: Padding(
+      padding: const EdgeInsets.all(10),
+      child: ScrollOnExpand(
+        child: Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            children: <Widget>[
+              ExpandablePanel(
+                controller: _controller,
+                theme: const ExpandableThemeData(
+                  headerAlignment: ExpandablePanelHeaderAlignment.center,
+                  tapBodyToExpand: true,
+                  tapBodyToCollapse: true,
+                  hasIcon: false,
+                ),
+                header: Container(
+                  color: Colors.indigoAccent,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        ExpandableIcon(
+                          theme: const ExpandableThemeData(
+                            expandIcon: Icons.arrow_right,
+                            collapseIcon: Icons.arrow_drop_down,
+                            iconColor: Colors.white,
+                            iconSize: 28.0,
+                            iconRotationAngle: math.pi / 2,
+                            iconPadding: EdgeInsets.only(right: 5),
+                            hasIcon: false,
+                          ),
+                        ),
+                        Expanded(
+                          child: StreamBuilder(
+                              stream: _section1Title,
+                              builder: (context, snapshot) => Text(
+                                    snapshot.data ?? '',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodySmall!
+                                        .copyWith(color: Colors.white),
+                                  )),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                collapsed: Container(),
+                expanded: buildList(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
 }
 
 const kSections = ['Purchase', 'Odometer & Tank', 'Notes & Attachments'];
